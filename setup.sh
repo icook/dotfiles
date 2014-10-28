be6d7633d38bfad5f206861c801e6c5c6075dbf5
Isaac Cook <isaac@simpload.com>
2014-10-28 12:44:49 -0500

    Adds mr config to managed configs

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -rfs $DIR/vimrc ~/.vimrc
 ln -rfs $DIR/gitconfig ~/.gitconfig
 ln -rfs $DIR/zshrc ~/.zshrc
+ln -rfs $DIR/mrconfig ~/.mrconfig
