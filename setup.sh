ce5191f3040b8a0b19d35367a41f5cd572692981
Isaac Cook <isaac@simpload.com>
2015-04-27 13:17:39 -0500

    Adds a neovim rc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -fs $DIR/vimrc ~/.vimrc
+ln -fs $DIR/nvimrc ~/.nvimrc
 ln -fs $DIR/gitconfig ~/.gitconfig
 ln -fs $DIR/zshrc ~/.zshrc
 ln -fs $DIR/mrconfig ~/.mrconfig
