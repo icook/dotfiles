62554ffaf6aba3bf22eca001b75f6847e07b5236
Isaac Cook <isaac@simpload.com>
2020-01-16 12:18:01 -0600

    Add terminator config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir ~/.config/terminator/
+ln -fs $DIR/home/.config/terminator/config ~/.config/terminator/config
+
 mkdir ~/.config/nvim/
 ln -fs $DIR/nvimrc ~/.config/nvim/init.vim
 
