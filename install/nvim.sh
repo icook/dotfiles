c7cfeb66630406a6b22263aea85d0ce0078e8581
Isaac Cook <isaac@simpload.com>
2020-01-17 15:09:02 -0600

    tweak install

--- install/nvim.sh
+++ install/nvim.sh
@@ -2,3 +2,4 @@
 OUT=$HOME/bin/nvim
 curl -L "https://github.com/neovim/neovim/releases/download/stable/nvim.appimage" -o $OUT
 chmod +x $OUT
+sudo apt-get install fonts-powerline -y
--- install/ubuntu-terminal.sh
+++ install/ubuntu-terminal.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ex
-sudo apt-get install zsh terminator git curl wget jq silversearcher-ag  -y
+sudo apt-get install zsh terminator git curl wget jq silversearcher-ag htop -y
